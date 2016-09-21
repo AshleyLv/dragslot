e2e3bf06664630db5ba587a5a0bface338c66af5
AshleyLicorice <ashley_lyf@icloud.com>
2016-09-21 16:16:00 +0800

    making slot can move from each slot
    1. making the slot item can move smoothly
    2. making placeEl detect the right position to display
    3. making slot can move from each slot

--- js/dragslot.js
+++ js/dragslot.js
@@ -1,11 +1,26 @@
 ;(function($, window, document, undefined){
-	var eStart  = 'mousedown touchstart MSPointerDown pointerdown',
-	eMove   = 'mousemove touchmove MSPointerMove pointermove',
-	eEnd    = 'mouseup touchend touchcancel MSPointerUp MSPointerCancel pointerup pointercancel';
-	var clientX,clientY,moving;
+	var eStart  = 'mousedown',
+	eMove   = 'mousemove',
+	eEnd    = 'mouseup';
+	
+	var clientX,clientY;
 	var defaults = {
 
 	}
+	var hasPointerEvents = (function()
+    {
+        var el    = document.createElement('div'),
+            docEl = document.documentElement;
+        if (!('pointerEvents' in el.style)) {
+            return false;
+        }
+        el.style.pointerEvents = 'auto';
+        el.style.pointerEvents = 'x';
+        docEl.appendChild(el);
+        var supports = window.getComputedStyle && window.getComputedStyle(el, '').pointerEvents === 'auto';
+        docEl.removeChild(el);
+        return !!supports;
+    })();
 	function Dragslot(element,options){
 		this.w  = $(window);
 		this.el = $(element);
@@ -18,18 +33,16 @@
 			slotContainer.placeEl = $('<div class="placeEl"/>');
 			var onStartEvent = function(e){
 				var handle = $(e.target);
-				if(!handle.hasClass('slot-item')){
+				if(!handle.hasClass('slot-item') && !handle.parent().hasClass('slot-item')){
 					return;
 				}
 
 				e.preventDefault();
 				slotContainer.dragStart(e);
+				
 			};
 			var onMoveEvent = function(e){
 				var handle = $(e.target);
-				if(!handle.hasClass('slot-item')){
-					return;
-				}
 				if(slotContainer.dragEl){
 						e.preventDefault();
 						slotContainer.dragMove(e);
@@ -53,6 +66,7 @@
 			dragItem = target.closest('.slot-item');
 			this.placeEl.css('height', dragItem.height());
 			this.dragEl = $(document.createElement('div')).addClass('slot-item drag-item');
+			this.slotlist = target.closest('.slot-list');
 			dragItem.after(this.placeEl);
 			var w = dragItem.width();
 			dragItem.css('width',w + 'px');
@@ -67,8 +81,6 @@
 				'left' : clientX,
 				'top'  : clientY
 			});
-			
-			moving = !0;
 		},
 		dragMove : function(e){
 			var newClientX = e.clientX,
@@ -79,11 +91,50 @@
 			this.dragEl[0].style.top = top + (newClientY - clientY) + 'px';
 			clientX = newClientX;
 			clientY = newClientY;
+			var isEmpty;
+			 // if (!hasPointerEvents) {
+                this.dragEl[0].style.visibility = 'hidden';
+            // }
+			this.pointEl = $(document.elementFromPoint(e.pageX - document.body.scrollLeft, e.pageY - (window.pageYOffset || document.documentElement.scrollTop)));
+			// if (!hasPointerEvents) {
+                this.dragEl[0].style.visibility = 'visible';
+            // }
+			if (this.pointEl.hasClass('slot-handler')) {
+                this.pointEl = this.pointEl.parent('li');
+            }
+            if (this.pointEl.hasClass('empty-slot')) {
+                isEmpty = true;
+            }
+             else if (!this.pointEl.length || !this.pointEl.hasClass('slot-item')) {
+                return;
+            }
+             var before = e.pageY < (this.pointEl.offset().top + this.pointEl.height() / 2);
+                    parent = this.placeEl.parent();
+
+             if (isEmpty) {
+                    list = $(document.createElement('lo')).addClass('slot-list');
+                    list.append(this.placeEl);
+                    this.pointEl.append(list);
+                    list.closest('.slot').removeClass('empty-slot');
+                }
+                else if (before) {
+                    this.pointEl.before(this.placeEl);
+                }
+                else {
+                    this.pointEl.after(this.placeEl);
+                }
 		},
 		dragEnd : function(e){
 			var el = this.dragEl.children('.slot-item').first();
             el[0].parentNode.removeChild(el[0]);
             this.placeEl.replaceWith(el);
+            this.dragEl.remove();
+            this.dragEl = null;
+            this.pointEl = null;
+            if(this.slotlist.children().length==0){
+            	this.slotlist.closest('.slot').addClass('empty-slot');
+            	this.slotlist[0].parentNode.removeChild(this.slotlist[0]);
+            }
 		}
 	}
 	
@@ -93,4 +144,4 @@
 		new Dragslot(slot,options);
 	}
 
-})(window.jQuery || window.Zepto, window, document);
+})(window.jQuery, window, document);
